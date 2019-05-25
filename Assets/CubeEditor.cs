dc889f7e417cbde3913f03171aa1cc47e25e1dc4
Ayman Moukhtar <ayman.moukhtar@outlook.com>
2019-05-25 19:21:20 +0200

    Initial commit, setting the basics of the world and static path following

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    [Range(1f, 10f)]
+    [SerializeField]
+    private float _dragOffset = 10f;
+
+    private TextMesh _textMesh;
+
+    private void Start()
+    {
+        _textMesh = GetComponentInChildren<TextMesh>();
+        AdjustNameAndLabelText();
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.RoundToInt(transform.position.x / _dragOffset) * _dragOffset,
+            0f,
+            Mathf.RoundToInt(transform.position.z / _dragOffset) * _dragOffset
+            );
+        AdjustNameAndLabelText();
+    }
+
+    private void AdjustNameAndLabelText()
+    {
+        var text = $"{transform.position.x / _dragOffset},{transform.position.z / _dragOffset}";
+        _textMesh.text = text;
+        gameObject.name = text;
+    }
+}
---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField]
+    private List<Waypoint> _path;
+
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    private IEnumerator FollowPath()
+    {
+        yield return new WaitForSeconds(1);
+        foreach (var waypoint in _path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
